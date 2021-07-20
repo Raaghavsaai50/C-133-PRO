9fe7bb5dc6a5651bfdb02c6f73d10f1428c60d8e
Raaghavsaai50 <77572078+Raaghavsaai50@users.noreply.github.com>
2021-07-20 13:39:42 +0530

    Add files via upload

---
+++ Bottle.js
@@ -0,0 +1,45 @@
+Objects=[];
+status="";
+function preload(){
+    img=loadImage("Bottle.jpg");
+    }
+    
+    function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Status : detecting objects";
+    }
+    function modelLoaded(){
+        console.log("model is loaded");
+        status=true;
+        objectDetector.detect(img,gotresults);
+    }
+    
+    function gotresults(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        Objects=results;
+    }
+    }
+    
+    function draw(){
+    image(img,0,0,640,420);
+    if(status !=""){
+        for(p=0;p<Objects.length;p++){
+            document.getElementById("status").innerHTML="Status : Objects detected";
+            fill("#FF0000");
+            percent=floor(Objects[p].confidence * 100);
+            text(Objects[p].label+" "+percent+"%",Objects[p].x,Objects[p].y);
+            noFill()
+            stroke("#FF0000");
+            rect(Objects[p].x,Objects[p].y,Objects[p].width,Objects[p].height);
+            }
+    }
+    }
+    function back(){
+        window.location="index.html";
+    }
---
+++ main.js
@@ -0,0 +1,12 @@
+function TV_btn(){
+    window.location="tv.html";
+}
+function Desk_btn(){
+    window.location="Desk.html";
+}
+function Bottle_btn(){
+    window.location="Bottle.html";
+}
+function Fruit_basket_btn(){
+    window.location="Fruit_basket.html";
+}
